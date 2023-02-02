95a5ff5f8289c571fe215eac350b5aa032d55c3c
sml <smlrodrigues.dev@gmail.com>
2023-02-02 09:04:00 -0300

    Add handles methods for the GeneralInfo component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CVForm from "./components/CVForm";
 
 class App extends React.Component {
   constructor() {
@@ -24,9 +25,43 @@ class App extends React.Component {
     }
   }
 
+  handleChangeName = (e) => {
+    this.setState({
+      generalInfo: {
+        name: e.target.value,
+        email: this.state.generalInfo.email,
+        phoneNumber: this.state.generalInfo.phoneNumber,
+      }
+    });
+  }
+
+  handleChangeEmail = (e) => {
+    this.setState({
+      generalInfo: {
+        name: this.state.generalInfo.name,
+        email: e.target.value,
+        phoneNumber: this.state.generalInfo.phoneNumber,
+      }
+    });
+  }
+
+  handleChangePhoneNumber = (e) => {
+    this.setState({
+      generalInfo: {
+        name: this.state.generalInfo.name,
+        email: this.state.generalInfo.email,
+        phoneNumber: e.target.value,
+      }
+    });
+  }
+
   render() {
+
     return (
       <div>
+        <CVForm handleChangeName={this.handleChangeName}
+                handleChangeEmail={this.handleChangeEmail}
+                handleChangePhoneNumber={this.handleChangePhoneNumber}/>
       </div>
     )
   }
