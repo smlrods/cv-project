895bd683006ed246b7fe7b49fe4749a2790ee694
sml <smlrodrigues.dev@gmail.com>
2023-02-02 07:54:36 -0300

    Add needed states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-function App() {
-  return (
-    <div className="App">
+import React from "react";
 
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      generalInfo: {
+        name: '',
+        email: '',
+        phoneNumber: '',
+      },
+      educationalExp: {
+        schoolName: '',
+        titleOfStudy: '',
+        dateOfStudy: '',
+      },
+      practicalExp: {
+        companyName: '',
+        positionTitle: '',
+        mainTasks: '',
+        dateFrom: '',
+        dateUntil: '',
+      } 
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
 }
 
 export default App;
